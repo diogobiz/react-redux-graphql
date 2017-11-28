92b5d79b4f2b6bec4a3a83f3bf67acc62ccaf59b
Munir Ahmed <elsangedy@gmail.com>
2017-11-28 19:06:44 -0200

    Create base css to remove margins

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App'
 
 import registerServiceWorker from './registerServiceWorker'
 
+import './assets/css/index.css'
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
