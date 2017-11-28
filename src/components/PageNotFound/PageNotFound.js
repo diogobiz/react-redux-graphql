72d27f03eeba1c310414cd5560a1798089c2632f
Munir Ahmed <elsangedy@gmail.com>
2017-11-28 18:52:19 -0200

    Create page not found component

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PageNotFound = (props) => (
+  <div>
+    <h1>404</h1>
+    <h3>Page not found!</h3>
+  </div>
+)
+
+export default PageNotFound
--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -10,14 +10,14 @@ import { RestrictedRoute } from '../../containers/RestrictedRoute'
 // const asyncLogin = asyncComponent(() => import('../../containers/Auth/Login'))
 // const asyncLogout = asyncComponent(() => import('../../containers/Auth/Logout'))
 // const asyncCounter = asyncComponent(() => import('../../containers/Counter/Counter'))
-// const asyncPageNotFound = asyncComponent(() => import('../PageNotFound/PageNotFound'))
+const asyncPageNotFound = asyncComponent(() => import('../PageNotFound/PageNotFound'))
 
 const Routes = [
   // <RestrictedRoute type="private" path="/" exact component={asyncApp} />,
   // <RestrictedRoute type="private" path="/counter" exact component={asyncCounter} />,
   // <RestrictedRoute type="private" path="/logout" exact component={asyncLogout} />,
   // <RestrictedRoute type="public" path="/login" exact key="appRoute@/signin" component={asyncLogin} />,
-  // <Route component={asyncPageNotFound} />
+  <Route component={asyncPageNotFound} />
 ]
 
 export default Routes
