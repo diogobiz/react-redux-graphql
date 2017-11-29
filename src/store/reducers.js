0ca13ec802d80bc7606764cf8b998c34b95482a0
Munir Ahmed <elsangedy@gmail.com>
2017-11-29 09:31:35 -0200

    Remove unnecessary dependencies

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,16 +1,10 @@
 import { combineReducers } from 'redux'
 
-import { responsiveStateReducer } from 'redux-responsive'
-
-import responsiveDrawer from 'material-ui-responsive-drawer/lib/store/reducer'
-
 import counter from './counter/reducer'
 import theme from './theme/reducer'
 import locale from './locale/reducer'
 
 export default combineReducers({
-  browser: responsiveStateReducer,
-  responsiveDrawer,
   counter,
   theme,
   locale
