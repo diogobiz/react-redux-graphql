5f3eee88005307d4f0cc0be14b631001a64d843e
Munir Ahmed <elsangedy@gmail.com>
2017-12-02 20:02:22 -0200

    Return name with capitalize

--- server/src/schema/pokemons/type.js
+++ server/src/schema/pokemons/type.js
@@ -2,6 +2,7 @@ import { GraphQLObjectType, GraphQLInt, GraphQLString } from 'graphql'
 
 const getUrl = (id) => `http://pokeapi.co/api/v2/pokemon/${id}/`
 const getImage = (id) => `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+const capitalaize = (str) => str.toLowerCase().split(' ').map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')
 
 const PokemonType = new GraphQLObjectType({
   name: 'Pokemon',
@@ -12,7 +13,8 @@ const PokemonType = new GraphQLObjectType({
       type: GraphQLInt
     },
     name: {
-      type: GraphQLString
+      type: GraphQLString,
+      resolve: (res) => capitalaize(res.name)
     },
     url: {
       type: GraphQLString,
