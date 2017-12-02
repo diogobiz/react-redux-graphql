d66f89ced4055685ab75e9ab0f589ffd7f4f7d56
Munir Ahmed <elsangedy@gmail.com>
2017-12-02 20:03:38 -0200

    Add route do Pokemons

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -7,10 +7,12 @@ import { asyncComponent } from '../../hoc/asyncComponent'
 import { RestrictedRoute } from '../../containers/RestrictedRoute'
 
 const asyncCounter = asyncComponent(() => import('../../containers/Counter/Counter'))
+const asyncPokemons = asyncComponent(() => import('../../containers/Pokemons/Pokemons'))
 const asyncPageNotFound = asyncComponent(() => import('../PageNotFound/PageNotFound'))
 
 const Routes = [
   <RestrictedRoute type="private" path="/counter" exact component={asyncCounter} />,
+  <RestrictedRoute type="private" path="/pokemons" exact component={asyncPokemons} />,
   <Route component={asyncPageNotFound} />
 ]
 
